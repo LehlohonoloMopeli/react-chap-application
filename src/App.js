44bee5026d69d909af01625a806d64163eb3ee8f
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-02 01:55:27 +0200

    Added src/

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID="36613617-517c-4915-b5b9-04fb03edae95"
+            userName = "lehlohonolompl"
+            userSecret = "123456"
+        />
+    );
+}
+
+export default App;
