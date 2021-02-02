fca7d5123de7d9a1867fac2b9787650d8a66d095
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-02 14:37:29 +0200

    Modified src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
+import ChatFeed from "./components/ChatFeed";
+
 const App = () => {
     return (
         <ChatEngine
@@ -8,6 +10,8 @@ const App = () => {
             projectID="36613617-517c-4915-b5b9-04fb03edae95"
             userName = "lehlohonolompl"
             userSecret = "123456"
+
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}       // Create our own component "ChatFeed", then spread out all the components coming from the chat feed via ""...chatAppProps"
         />
     );
 }
