f4b6d08b7e57fd302f6ff63b44479ac7a1aee26c
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-06 16:21:38 +0200

    added icons for sending messages and image upload

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';      //These functions come pre-Built
+import { SendOutlined, PictureOutlined} from '@ant-design/icons';
 
 // Will be used for sending messages.
-function MessageForm() {
+function MessageForm(props) {
+
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const text = value.trim();
+
+        if(text.length > 0 ) sendMessage( creds, chatId, { text });
+
+        setValue('');
+    }
+
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+        isTyping(props, chatId);
+    }
+
+
+    const handleUpload = (event) => {
+        sendMessage(props, chatId, { files : event.target.files, text : '' })
+    }
+
+
     return (
-        <div>
-            Message Form
-        </div>
+        <form className='message-form' onSubmit={handleSubmit}>
+            <input
+                className='message-input'
+                placeholder="Don't be boring..."
+                value={value}
+                onChange={handleChange}
+            />
+            <label htmlFor='upload-button'>
+                <span className='image-button'>
+                    <PictureOutlined className='picture-icon'/>     {/* Icon for uploading images */}
+                </span>
+            </label>
+            <input
+                type='file'
+                multiple={false}
+                id='upload-button'
+                style={{ display : 'none'}}
+                onChange={handleUpload}
+            />
+            <button type='submit' className='send-button'>
+                <SendOutlined className='send-icon'/>
+            </button>
+        </form>
     )
 }
 
