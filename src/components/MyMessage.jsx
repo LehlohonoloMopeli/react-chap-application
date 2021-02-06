3a3f229fae27069d25072db372fb508895394246
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-06 13:47:19 +0200

    Customised the MyMessage component

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import MessageForm from './MessageForm';
 
 // The message bubble that we send.
-function MyMessage() {
+function MyMessage({message}) {
+
+    // First check if the message is an image
+    if(message?.attachments?.length > 0) {
+        return (
+            <img
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className='message-image'
+                style={{ float : 'right' }}
+            />
+        )
+    }
+
     return (
-        <div>
-            Our message bubble.
+        <div className="message" style={{ float : 'right', marginRight : '18px', color : 'white', backgroundColor : '#0080ff'}}>
+            {message.text}
         </div>
     )
 }
