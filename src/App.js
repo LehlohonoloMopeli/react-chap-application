cc0207870b7b4bc592b803aebac280870649561e
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-06 18:41:20 +0200

    Modified src/App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
+
+    if(!localStorage.getItem('username')) {
+        return <LoginForm/>
+    }
+
     return (
         <ChatEngine
             height = "100vh"
             projectID="36613617-517c-4915-b5b9-04fb03edae95"
-            userName = "lehlohonolompl"
-            userSecret = "123456"
+            userName = { localStorage.getItem('username') }
+            userSecret = { localStorage.getItem('password') }
 
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}       // Create our own component "ChatFeed", then spread out all the components coming from the chat feed via ""...chatAppProps"
         />
